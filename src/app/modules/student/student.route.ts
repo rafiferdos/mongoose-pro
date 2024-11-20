b768b608103400680f98f5b578fb04b1b219fdd3
Rafi Ferdos <rafiferdos@gmail.com>
2024-11-20 18:02:59 +0600

    feat: add route to fetch all students from the database

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -4,5 +4,6 @@ import { StudentController } from './student.controller'
 const router = express.Router()
 
 router.post('/create-student', StudentController.createStudent)
+router.get('/', StudentController.getAllStudentsFromDB)
 
 export const StudentRoutes = router
