7ae1b55f6dc1ee460bf1d86d81a31f4dee143d58
Rafi Ferdos <rafiferdos@gmail.com>
2024-12-04 21:56:27 +0600

    feat: define user interface with role and status types

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,11 @@
+export type TUserRole = 'admin' | 'student' | 'faculty'
+export type TUserStatus = 'in-progress' | 'blocked'
+
+export type TUser = {
+  id: string
+  password: string
+  needsPasswordChange: boolean
+  role: TUserRole
+  isDeleted: boolean
+  status: TUserStatus
+}
