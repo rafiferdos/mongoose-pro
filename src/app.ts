fd44a836506ddd78cd9e3445854d68f683304efe
Rafi Ferdos <rafiferdos@gmail.com>
2024-12-05 23:08:23 +0600

    feat: refactor routing structure by consolidating student and user routes into a single router

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
-import { StudentRoutes } from './app/modules/student/student.route'
-import { userRoutes } from './app/modules/user/user.route'
+import router from './app/routes'
 
 const app: Application = express()
 
@@ -10,14 +9,12 @@ app.use(express.json())
 app.use(cors())
 
 //application routes
-app.use('/api/v1/students', StudentRoutes)
-app.use('/api/v1/users', userRoutes)
-
+app.use('/api/v1', router)
 app.get('/', function (req: Request, res: Response) {
   res.send('Hello World')
 })
 
 app.use(globalErrorHandler)
 
-app.listen(3000)
+// app.listen(9000)
 export default app
