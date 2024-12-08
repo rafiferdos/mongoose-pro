4cd1e1fcacc26524f63cd748ee670f9b42042294
Rafi Ferdos <rafiferdos@gmail.com>
2024-12-08 18:14:49 +0600

    feat: implement AcademicSemester model with Mongoose schema and type aliases

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -1,7 +1,39 @@
-import { Schema } from 'mongoose';
-import { AcademicSemester } from './academicSemester.interface';
+import { model, Schema } from 'mongoose'
+import {
+  TAcademicSemester,
+  TMonth,
+  TSemesterCode,
+} from './academicSemester.interface'
 
+export const AcademicSemesterSchema = new Schema<TAcademicSemester>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: Date,
+      required: true,
+    },
+    code: {
+      type: String,
+      enum: [] as TSemesterCode[],
+    },
+    startMonth: {
+      type: String,
+      required: true,
+      enum: [] as TMonth[],
+    },
+    endMonth: {
+      type: String,
+      required: true,
+      enum: [] as TMonth[],
+    },
+  },
+  { timestamps: true },
+)
 
-const AcademicSemesterSchema = new Schema<> {
-    name: 
-}
+export const AcademicSemesterModel = model<TAcademicSemester>(
+  'AcademicSemester',
+  AcademicSemesterSchema,
+)
