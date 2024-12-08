184e74dd61e38054c981909323066c90be3980e9
Rafi Ferdos <rafiferdos@gmail.com>
2024-12-08 19:29:58 +0600

    feat: add TAcademicSemesterNameCodeMapper type and validate academic semester name against code in createAcademicSemesterIntoDB

--- src/app/modules/academicSemester/academicSemester.interface.ts
+++ src/app/modules/academicSemester/academicSemester.interface.ts
@@ -21,3 +21,7 @@ export type TAcademicSemester = {
   startMonth: TMonth
   endMonth: TMonth
 }
+
+export type TAcademicSemesterNameCodeMapper = {
+  [key: string]: string
+}
--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,8 +1,21 @@
-import { TAcademicSemester } from "./academicSemester.interface";
-import { AcademicSemesterModel } from "./academicSemester.model";
+import {
+  TAcademicSemester,
+  TAcademicSemesterNameCodeMapper,
+} from './academicSemester.interface'
+import { AcademicSemesterModel } from './academicSemester.model'
 
-const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => await AcademicSemesterModel.create(payload);
+const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
+  const academicSemesterNameCodeMapper: TAcademicSemesterNameCodeMapper = {
+    Autumn: '01',
+    Summer: '02',
+    Fall: '03',
+  }
+  if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
+    throw new Error('Invalid Academic Semester Name')
+  }
+  return await AcademicSemesterModel.create(payload)
+}
 
 export const academicSemesterServices = {
-    createAcademicSemesterIntoDB,
+  createAcademicSemesterIntoDB,
 }
