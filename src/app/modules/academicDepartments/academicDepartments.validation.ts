8ee0a460c3816889192465cd153117e4c58cedf6
Rafi Ferdos <rafiferdos@gmail.com>
2024-12-15 15:38:53 +0600

    refactor: format validation schemas for academic departments

--- src/app/modules/academicDepartments/academicDepartments.validation.ts
+++ src/app/modules/academicDepartments/academicDepartments.validation.ts
@@ -1,28 +1,28 @@
 import { z } from 'zod'
 
 const createAcademicDepartmentValidationSchema = z.object({
-    body: z.object({
-        name: z.string({
-            invalid_type_error: 'Name must be a string',
-        }),
-        academicFaculty: z.string({
-            invalid_type_error: 'Academic Faculty must be a string',
-        }),
-    })
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Name must be a string',
+    }),
+    academicFaculty: z.string({
+      invalid_type_error: 'Academic Faculty must be a string',
+    }),
+  }),
 })
 
 const updateAcademicDepartmentValidationSchema = z.object({
-    body: z.object({
-        name: z.string({
-            invalid_type_error: 'Name should be a string to update',
-        }),
-        academicFaculty: z.string({
-            invalid_type_error: 'Academic Faculty should be a string to update',
-        }),
-    })
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Name should be a string to update',
+    }),
+    academicFaculty: z.string({
+      invalid_type_error: 'Academic Faculty should be a string to update',
+    }),
+  }),
 })
 
 export const AcademicDepartmentValidations = {
-    createAcademicDepartmentValidationSchema,
-    updateAcademicDepartmentValidationSchema,
+  createAcademicDepartmentValidationSchema,
+  updateAcademicDepartmentValidationSchema,
 }
