b84fa832f41c1b4b95511c4c65eebbaf962d6ef6
Rafi Ferdos <rafiferdos@gmail.com>
2024-12-15 13:47:04 +0600

    feat: add validation schemas for creating and updating academic departments

---
+++ src/app/modules/academicDepartments/academicDepartments.validation.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod'
+
+const createAcademicDepartmentValidationSchema = z.object({
+    body: z.object({
+        name: z.string({
+            invalid_type_error: 'Name must be a string',
+        })
+    })
+})
+
+const updateAcademicDepartmentValidationSchema = z.object({
+    body: z.object({
+        name: z.string({
+            invalid_type_error: 'Name should be a string to update',
+        })
+    })
+})
+
+export const AcademicDepartmentValidations = {
+    createAcademicDepartmentValidationSchema,
+    updateAcademicDepartmentValidationSchema,
+}
