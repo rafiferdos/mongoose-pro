ea39cac44c5547076ac268b5ad45edb3a41c4302
Rafi Ferdos <rafiferdos@gmail.com>
2024-12-15 23:57:36 +0600

    feat: enhance global error handling and implement AppError class for better error management

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,10 +1,24 @@
 import { Student } from './student.model'
 
 const getAllStudentsFromDB = async () =>
-  await Student.find().populate('academicDepartment').populate('academicSemester')
+  await Student.find()
+    .populate({
+      path: 'academicDepartment',
+      populate: {
+        path: 'academicFaculty',
+      },
+    })
+    .populate('academicSemester')
 
 const getSingleStudentFromDB = async (id: string) =>
-  await Student.findOne({id: id}).populate('academicDepartment', 'academicSemester')
+  await Student.findOne({ id: id })
+    .populate({
+      path: 'academicDepartment',
+      populate: {
+        path: 'academicFaculty',
+      },
+    })
+    .populate('academicSemester')
 
 const deleteStudent = async (id: string) =>
   await Student.updateOne({ id: id }, { isDeleted: true })
