6d833ea4275252888e0847b59e8fe2f89bfab698
Rafi Ferdos <rafiferdos@gmail.com>
2024-12-15 16:02:27 +0600

    feat: add route for creating an academic department with validation

--- src/app/modules/academicDepartments/academicDepartments.route.ts
+++ src/app/modules/academicDepartments/academicDepartments.route.ts
@@ -1,3 +1,14 @@
 import express from 'express'
+import { validateRequest } from '../../middlewares/validateRequest'
+import { academicDepartmentControllers } from './academicDepartments.controller'
+import { AcademicDepartmentValidations } from './academicDepartments.validation'
 
 const router = express.Router()
+
+router.post(
+  '/create-academic-department',
+  validateRequest(
+    AcademicDepartmentValidations.createAcademicDepartmentValidationSchema,
+  ),
+  academicDepartmentControllers.createAcademicDepartment,
+)
