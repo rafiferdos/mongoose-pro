29a66e6dde695a7886d9884408fe930145959575
Rafi Ferdos <rafiferdos@gmail.com>
2024-12-15 15:49:45 +0600

    refactor: update academic department service to use academicDepartmentModel and add CRUD operations

--- src/app/modules/academicDepartments/academicDepartments.service.ts
+++ src/app/modules/academicDepartments/academicDepartments.service.ts
@@ -1,4 +1,23 @@
-import { TAcademicDepartment } from "./academicDepartments.interface";
-import { academicDepartment } from "./academicDepartments.model";
+import { TAcademicDepartment } from './academicDepartments.interface'
+import { academicDepartmentModel } from './academicDepartments.model'
 
-const createAcademicDepartmentIntoDB = async(payload: TAcademicDepartment) => await academicDepartment
+const createAcademicDepartmentIntoDB = async (payload: TAcademicDepartment) =>
+  await academicDepartmentModel.create(payload)
+
+const getAllAcademicDepartmentsFromDB = async () =>
+  await academicDepartmentModel.find()
+
+const getSingleAcademicDepartmentFromDB = async (id: string) =>
+  await academicDepartmentModel.findById(id)
+
+const updateAcademicDepartmentInDB = async (
+  id: string,
+  payload: Partial<TAcademicDepartment>,
+) => await academicDepartmentModel.findByIdAndUpdate(id, payload, { new: true })
+
+export const academicDepartmentService = {
+  createAcademicDepartmentIntoDB,
+  getAllAcademicDepartmentsFromDB,
+  getSingleAcademicDepartmentFromDB,
+  updateAcademicDepartmentInDB,
+}
