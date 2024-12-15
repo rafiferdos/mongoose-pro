88a2f55d84bc2eaca286803711751d792b9c1c18
Rafi Ferdos <rafiferdos@gmail.com>
2024-12-15 12:55:00 +0600

    feat: add routes for creating, retrieving, and updating academic faculty

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -1,4 +1,34 @@
-import express from 'express';
+import express from 'express'
+import { validateRequest } from './../../middlewares/validateRequest'
+import { academicFacultyController } from './academicFaculty.controller'
+import { academicFacultyValidation } from './academicFaculty.validation'
 
-const router = express.Router();
+const router = express.Router()
 
+router.post(
+  '/create-academic-faculty',
+  validateRequest(
+    academicFacultyValidation.createAcademicFacultyValidationSchema,
+  ),
+  academicFacultyController.createAcademicFaculty,
+)
+
+router.get(
+  '/get-all-academic-faculties',
+  academicFacultyController.getAllAcademicFaculties,
+)
+
+router.get(
+  '/get-single-academic-faculty/:id',
+  academicFacultyController.getSingleAcademicFaculty,
+)
+
+router.patch(
+  '/update-academic-faculty/:id',
+  validateRequest(
+    academicFacultyValidation.updateAcademicFacultyValidationSchema,
+  ),
+  academicFacultyController.updateAcademicFaculty,
+)
+
+export default router
