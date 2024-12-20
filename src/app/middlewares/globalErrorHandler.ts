4c915aed59e8d1ea83a471c482afdc3aa58964c4
Rafi Ferdos <rafiferdos@gmail.com>
2024-12-20 19:23:55 +0600

    feat: enhance global error handler to include structured error sources for better error reporting

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -5,10 +5,24 @@ import { ErrorRequestHandler } from 'express'
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   const statusCode = err?.statusCode || 500
   const message = err.message || 'Something went wrong'
+
+  type TErrorSource = {
+    path: string | number
+    message: string
+  }[]
+
+  const errorSources: TErrorSource = [
+    {
+      path: '',
+      message: '',
+    },
+  ]
+
   res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    // error: err,
+    errorSources,
   })
 }
 
