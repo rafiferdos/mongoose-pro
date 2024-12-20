b98824b8789f1148c790dbfd10312966112249d7
Rafi Ferdos <rafiferdos@gmail.com>
2024-12-20 19:38:31 +0600

    feat: add validation error handlers for mongoose and Zod to standardize error responses

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,63 +1,63 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-unused-vars */
-import { ErrorRequestHandler } from 'express';
-import { ZodError } from 'zod';
-import config from '../config';
-import AppError from '../errors/AppError';
-import handleCastError from '../errors/handleCastError';
-import handleDuplicateError from '../errors/handleDuplicateError';
-import handleValidationError from '../errors/handleValidationError';
-import handleZodError from '../errors/handleZodError';
-import { TErrorSources } from '../interface/error';
+
+import { ErrorRequestHandler } from 'express'
+import { ZodError } from 'zod'
+import config from '../config'
+import AppError from '../errors/AppError'
+import handleCastError from '../errors/handleCastError'
+import handleDuplicateError from '../errors/handleDuplicateError'
+import handleValidationError from '../errors/handleValidationError'
+import handleZodError from '../errors/handleZodError'
+import { TErrorSources } from '../interface/error'
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   //setting default values
-  let statusCode = 500;
-  let message = 'Something went wrong!';
+  let statusCode = 500
+  let message = 'Something went wrong!'
   let errorSources: TErrorSources = [
     {
       path: '',
       message: 'Something went wrong',
     },
-  ];
+  ]
 
   if (err instanceof ZodError) {
-    const simplifiedError = handleZodError(err);
-    statusCode = simplifiedError?.statusCode;
-    message = simplifiedError?.message;
-    errorSources = simplifiedError?.errorSources;
+    const simplifiedError = handleZodError(err)
+    statusCode = simplifiedError?.statusCode
+    message = simplifiedError?.message
+    errorSources = simplifiedError?.errorSources
   } else if (err?.name === 'ValidationError') {
-    const simplifiedError = handleValidationError(err);
-    statusCode = simplifiedError?.statusCode;
-    message = simplifiedError?.message;
-    errorSources = simplifiedError?.errorSources;
+    const simplifiedError = handleValidationError(err)
+    statusCode = simplifiedError?.statusCode
+    message = simplifiedError?.message
+    errorSources = simplifiedError?.errorSources
   } else if (err?.name === 'CastError') {
-    const simplifiedError = handleCastError(err);
-    statusCode = simplifiedError?.statusCode;
-    message = simplifiedError?.message;
-    errorSources = simplifiedError?.errorSources;
+    const simplifiedError = handleCastError(err)
+    statusCode = simplifiedError?.statusCode
+    message = simplifiedError?.message
+    errorSources = simplifiedError?.errorSources
   } else if (err?.code === 11000) {
-    const simplifiedError = handleDuplicateError(err);
-    statusCode = simplifiedError?.statusCode;
-    message = simplifiedError?.message;
-    errorSources = simplifiedError?.errorSources;
+    const simplifiedError = handleDuplicateError(err)
+    statusCode = simplifiedError?.statusCode
+    message = simplifiedError?.message
+    errorSources = simplifiedError?.errorSources
   } else if (err instanceof AppError) {
-    statusCode = err?.statusCode;
-    message = err.message;
+    statusCode = err?.statusCode
+    message = err.message
     errorSources = [
       {
         path: '',
         message: err?.message,
       },
-    ];
+    ]
   } else if (err instanceof Error) {
-    message = err.message;
+    message = err.message
     errorSources = [
       {
         path: '',
         message: err?.message,
       },
-    ];
+    ]
   }
 
   //ultimate return
@@ -67,10 +67,10 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     errorSources,
     err,
     stack: config.NODE_ENV === 'development' ? err?.stack : null,
-  });
-};
+  })
+}
 
-export default globalErrorHandler;
+export default globalErrorHandler
 
 //pattern
 /*
